eeff6120035ab9fd6ca5ec124fd0ea32fafa72ff
takuro-inoue <takuro.inoue@blueish.co.jp>
2021-11-03 15:16:52 +0900

    module, controller, serviceの作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ItemModule } from './item/item.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), ItemModule],
   controllers: [AppController],
   providers: [AppService],
 })
